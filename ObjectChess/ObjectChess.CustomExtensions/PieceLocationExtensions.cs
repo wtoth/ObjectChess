ebb24dba4292ca803473c956bb0f67c8697201ad
Billy Toth <wtoth@umich.edu>
2023-01-03 11:49:28 -0500

    Moved Algebraicnotationtorankfile & inverse to extensions

---
+++ ObjectChess/ObjectChess.CustomExtensions/PieceLocationExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ObjectChess.Models;
+
+namespace ObjectChess.CustomExtensions
+{
+    public static class PieceLocationExtensions
+    {
+        public static string RankFileToAlgebraicNotation(this PieceLocation rankfile)
+        {
+            string algnotation = "";
+            if (rankfile.File == 0)
+            {
+                algnotation = algnotation + "A";
+            }
+            else if (rankfile.File == 1)
+            {
+                algnotation = algnotation + "B";
+            }
+            else if (rankfile.File == 2)
+            {
+                algnotation = algnotation + "C";
+            }
+            else if (rankfile.File == 3)
+            {
+                algnotation = algnotation + "D";
+            }
+            else if (rankfile.File == 4)
+            {
+                algnotation = algnotation + "E";
+            }
+            else if (rankfile.File == 5)
+            {
+                algnotation = algnotation + "F";
+            }
+            else if (rankfile.File == 6)
+            {
+                algnotation = algnotation + "G";
+            }
+            else if (rankfile.File == 7)
+            {
+                algnotation = algnotation + "H";
+            }
+            algnotation = algnotation + (rankfile.Rank + 1).ToString();
+            return algnotation;
+        }
+    }
+}
---
+++ ObjectChess/ObjectChess.CustomExtensions/StringExtensions.cs
@@ -0,0 +1,48 @@
+﻿using ObjectChess.Models;
+
+namespace ObjectChess.CustomExtensions
+{
+    public static class StringExtensions
+    {
+        public static PieceLocation AlgebraicNotationToRankFile(this string input)
+        {
+            PieceLocation RankFile = new PieceLocation();
+            char[] letters = input.ToCharArray();
+            RankFile.Rank = (int)Char.GetNumericValue(letters[1]) - 1;
+
+            if (char.ToLower(letters[0]) == 'a')
+            {
+                RankFile.File = 0;
+            }
+            else if (char.ToLower(letters[0]) == 'b')
+            {
+                RankFile.File = 1;
+            }
+            else if (char.ToLower(letters[0]) == 'c')
+            {
+                RankFile.File = 2;
+            }
+            else if (char.ToLower(letters[0]) == 'd')
+            {
+                RankFile.File = 3;
+            }
+            else if (char.ToLower(letters[0]) == 'e')
+            {
+                RankFile.File = 4;
+            }
+            else if (char.ToLower(letters[0]) == 'f')
+            {
+                RankFile.File = 5;
+            }
+            else if (char.ToLower(letters[0]) == 'g')
+            {
+                RankFile.File = 6;
+            }
+            else if (char.ToLower(letters[0]) == 'h')
+            {
+                RankFile.File = 7;
+            }
+            return RankFile;
+        }
+    }
+}
